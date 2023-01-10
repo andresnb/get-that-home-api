ab90bb8af9c4cc3958b5dd66cdaa459860ab110d
Andres Naula <andresnaula@gmail.com>
2023-01-10 10:34:00 -0500

    add: test seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+User.create(email:"test@mail.com", password:"123456")
+User.create(email:"hello@mail.com", password:"123456")
