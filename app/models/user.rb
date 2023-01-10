397beddbdad69cb6d0db68daa7a90927c78a13f9
Andres Naula <andresnaula@gmail.com>
2023-01-10 10:42:46 -0500

    add: user validation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_secure_token
   has_secure_password
+
+  def invalidate_token
+    update(token: nil)
+  end
+
+  def self.valid_login?(username, password)
+    user = find_by(username: username)
+    user&.authenticate(password)
+  end
 end
