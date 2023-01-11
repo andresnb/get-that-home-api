1d0c0708daec0b2d91e38e972d0e237620638294
Andres Naula <andresnaula@gmail.com>
2023-01-11 11:48:20 -0500

    WIP: restructuring properties

--- db/migrate/20230111162209_create_properties.rb
+++ db/migrate/20230111162209_create_properties.rb
@@ -1,7 +1,7 @@
 class CreateProperties < ActiveRecord::Migration[7.0]
   def change
     create_table :properties do |t|
-      t.integer :owner_id
+      t.references :user_id, null: false, foreign_key: true
       t.string :name
       t.string :addresse
       t.string :district
@@ -13,7 +13,7 @@ class CreateProperties < ActiveRecord::Migration[7.0]
       t.integer :area
       t.boolean :pets
       t.money :price
-      t.money :maintanance
+      t.money :maintenance
       t.integer :operation_type
       t.integer :property_type
 
